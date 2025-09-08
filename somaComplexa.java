21b32a185b401bf8cf0fe519da6dea76a1a66733
Sidney Alves <sidneyalves412@gmail.com>
2025-09-08 11:19:32 -0300

    Add soma completa

---
+++ somaComplexa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SomaComplexa {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite Dois numeros:");
+        double a = 0;
+        double b = 0;
+        a = ler.nextDouble();
+        b = ler.nextDouble();
+        double soma = a + b;
+        System.out.println("SOMA = "+soma);
+        
+    }
+}
